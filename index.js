d7b18d8851890ee16a7d3c07898e6e98fb0680c6
Marcus Jun <mjun@calstate.edu>
2018-03-01 02:24:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,5 +21,7 @@ function sayHiToGrandma(string){
     return ("I can't hear you!")
   } else if (string.toUpperCase()===uppercase){
     return ("YES INDEED!")
+  } else if (string==="I love you, Grandma."){
+    return ("I love you, too.")
   }
 }
